d0177b67255c8b9573140789382bd226e5273df3
habibbinsync <habibbinsync@gmail.com>
2025-06-01 09:26:15 -0700

    Initial Project Setup

---
+++ BinSync.Blazor/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace BinSync.Blazor
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ BinSync.Blazor/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace BinSync.Blazor
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
